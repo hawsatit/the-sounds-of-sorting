352c4196f742ca3c79652e87a1ca3f24428587e8
fui <fui@fui-ubuntu>
2025-03-20 12:59:41 -0500

    implemented the events that implement the SortEvent interface

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
@@ -1,8 +1,32 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.List;
+
 /**
  * A <code>CopyEvent</code> logs a copy of a value into an index of the array.
+ * @param <T>
  */
-public class CopyEvent<T> {
-    // TODO: implement me!
+public class CopyEvent<T> implements SortEvent<T> {
+    private final T value;
+    private final int destination;
+
+    public CopyEvent(T value, int destination) {
+        this.value = value;
+        this.destination = destination;
+    }
+
+    @Override
+    public void apply(T[] arr) {
+        arr[destination] = value;
+    }
+
+    @Override
+    public List<Integer> getAffectedIndices() {
+        return List.of(destination);
+    }
+
+    @Override
+    public boolean isEmphasized() {
+        return true;
+    }
 }
