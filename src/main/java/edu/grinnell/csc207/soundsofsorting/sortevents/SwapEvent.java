63f6f16d8d0f19f6c8598bd7cd29b4163ceedd34
fui <fui@fui-ubuntu>
2025-03-20 12:59:50 -0500

    implemented the events that implement the SortEvent interface

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
@@ -1,8 +1,38 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.List;
+
 /**
  * A <code>SwapEvent</code> logs a swap between two indices of the array.
  */
-public class SwapEvent<T> {
-    // TODO: implement me
+public class SwapEvent<T> implements SortEvent<T> {
+    
+    private final int index1;
+    private final int index2;
+    
+    public SwapEvent(int index1, int index2) {
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+            
+    @Override
+    public void apply(T[] arr) {
+        T temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    @Override
+    public List<Integer> getAffectedIndices() {
+        return List.of(index1, index2);
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public boolean isEmphasized() {
+        return true;
+    }
 }
