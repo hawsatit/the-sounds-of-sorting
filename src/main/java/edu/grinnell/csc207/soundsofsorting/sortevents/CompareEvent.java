10780e6830a3835f2be8456eb494dad684f2bf5e
fui <fui@fui-ubuntu>
2025-03-20 12:59:37 -0500

    implemented the events that implement the SortEvent interface

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
@@ -1,9 +1,33 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A <code>CompareEvent</code> logs a comparison a sort makes between two
  * indices in the array.
+ * @param <T>
  */
-public class CompareEvent<T> {
-    // TODO: implement me!
+public class CompareEvent<T> implements SortEvent<T> {
+    private final int index1;
+    private final int index2;
+
+    public CompareEvent(int index1, int index2) {
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+
+    @Override
+    public void apply(T[] arr) {
+    }
+
+    @Override
+    public List<Integer> getAffectedIndices() {
+        return List.of(index1, index2);
+    }
+
+    @Override
+    public boolean isEmphasized() {
+        return false;
+    }
 }
