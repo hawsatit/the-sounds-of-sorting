abbcaad18bf062fcc42dd87e59d311d3f9a5bfc9
fui <fui@fui-ubuntu>
2025-03-19 22:09:15 -0500

    implemented tests

--- src/test/java/edu/grinnell/csc207/soundsofsorting/SortsTests.java
+++ src/test/java/edu/grinnell/csc207/soundsofsorting/SortsTests.java
@@ -32,8 +32,69 @@ public class SortsTests {
         };
     }
 
+    //original test on array of random numbers
     public void testSort(Consumer<Integer[]> func) {
         Integer[] arr = makeTestArray();
+        func.accept(arr);
+        assertTrue(sorted(arr));
+    }
+    
+    //test on array of numbers with multiple repeats
+    public void testMultipleRepeats(Consumer<Integer[]> func){
+        Integer[] arr = {0 , 4 , 6, 2, 2, 6, 4, 5, 5, 5, 0, 0};
+        func.accept(arr);
+        assertTrue(sorted(arr));
+    }
+    
+    //test on array of numbers with multiple repeats
+    public void testNegatives(Consumer<Integer[]> func){
+        Integer[] arr = {0 , -3, -27, -4, 6, 7, 2};
+        func.accept(arr);
+        assertTrue(sorted(arr));
+    }
+    
+    //test on array of ascneding numbers (already sorted)
+    public void testAscending(Consumer<Integer[]> func){
+        Integer[] arr = {0 , 1, 2, 3, 4, 5, 6, 7, 8};
+        func.accept(arr);
+        assertTrue(sorted(arr));
+    }
+    
+    //test on array of descending numbers
+    public void testDescending(Consumer<Integer[]> func){
+        Integer[] arr = {8, 7, 6, 5, 4, 3, 2, 1, 0};
+        func.accept(arr);
+        assertTrue(sorted(arr));
+    }
+    
+
+    //test on empty array
+    public void testEmptyArray(Consumer<Integer[]> func) {
+        Integer[] arr = {};
+        func.accept(arr);
+        assertTrue(sorted(arr));
+    }
+
+    //test on single-element array
+    public void testSingleElement(Consumer<Integer[]> func) {
+        Integer[] arr = {69};
+        func.accept(arr);
+        assertTrue(sorted(arr));
+    }
+
+    //test on array with all identical elements
+    public void testAllSameElements(Consumer<Integer[]> func) {
+        Integer[] arr = {7, 7, 7, 7, 7, 7, 7, 7};
+        func.accept(arr);
+        assertTrue(sorted(arr));
+    }
+    
+    //test on large array
+    public void testLargeArray(Consumer<Integer[]> func) {
+        Integer[] arr = new Integer[1000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 1000);
+        }
         func.accept(arr);
         assertTrue(sorted(arr));
     }
@@ -41,25 +102,78 @@ public class SortsTests {
     @Test
     public void testBubbleSort() {
         testSort(Sorts::bubbleSort);
+        testMultipleRepeats(Sorts::bubbleSort);
+        testNegatives(Sorts::bubbleSort);
+        testAscending(Sorts::bubbleSort);
+        testDescending(Sorts::bubbleSort);
+        testEmptyArray(Sorts::bubbleSort);
+        testSingleElement(Sorts::bubbleSort);
+        testAllSameElements(Sorts::bubbleSort);
+        testLargeArray(Sorts::bubbleSort);
     }
     
     @Test
     public void testInsertionSort() {
         testSort(Sorts::insertionSort);
+        testMultipleRepeats(Sorts::insertionSort);
+        testNegatives(Sorts::insertionSort);
+        testAscending(Sorts::insertionSort);
+        testDescending(Sorts::insertionSort);
+        testEmptyArray(Sorts::insertionSort);
+        testSingleElement(Sorts::insertionSort);
+        testAllSameElements(Sorts::insertionSort);
+        testLargeArray(Sorts::insertionSort);
     }
     
     @Test
     public void testSelectionSort() {
         testSort(Sorts::selectionSort);
+        testMultipleRepeats(Sorts::selectionSort);
+        testNegatives(Sorts::selectionSort);
+        testAscending(Sorts::selectionSort);
+        testDescending(Sorts::selectionSort);
+        testEmptyArray(Sorts::selectionSort);
+        testSingleElement(Sorts::selectionSort);
+        testAllSameElements(Sorts::selectionSort);
+        testLargeArray(Sorts::selectionSort);
     }
 
     @Test
     public void testMergeSort() {
         testSort(Sorts::mergeSort);
+        testMultipleRepeats(Sorts::mergeSort);
+        testNegatives(Sorts::mergeSort);
+        testAscending(Sorts::mergeSort);
+        testDescending(Sorts::mergeSort);
+        testEmptyArray(Sorts::mergeSort);
+        testSingleElement(Sorts::mergeSort);
+        testAllSameElements(Sorts::mergeSort);
+        testLargeArray(Sorts::mergeSort);
     }
     
     @Test
     public void testQuickSort() {
         testSort(Sorts::quickSort);
+        testMultipleRepeats(Sorts::quickSort);
+        testNegatives(Sorts::quickSort);
+        testAscending(Sorts::quickSort);
+        testDescending(Sorts::quickSort);
+        testEmptyArray(Sorts::quickSort);
+        testSingleElement(Sorts::quickSort);
+        testAllSameElements(Sorts::quickSort);
+        testLargeArray(Sorts::quickSort);
+    }
+    
+    @Test
+    public void testHeapSort() {
+        testSort(Sorts::heapSort);
+        testMultipleRepeats(Sorts::heapSort);
+        testNegatives(Sorts::heapSort);
+        testAscending(Sorts::heapSort);
+        testDescending(Sorts::heapSort);
+        testEmptyArray(Sorts::heapSort);
+        testSingleElement(Sorts::heapSort);
+        testAllSameElements(Sorts::heapSort);
+        testLargeArray(Sorts::heapSort);
     }
 }
