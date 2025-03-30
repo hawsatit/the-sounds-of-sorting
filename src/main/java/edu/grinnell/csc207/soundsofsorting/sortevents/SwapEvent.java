e17fe78219ae0fd484b121d8ed6dcd6eb0d4a5d1
fui <fui@fui-ubuntu>
2025-03-30 17:16:14 -0500

    Finished the program!

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/SwapEvent.java
@@ -7,15 +7,15 @@ import java.util.List;
  * A <code>SwapEvent</code> logs a swap between two indices of the array.
  */
 public class SwapEvent<T> implements SortEvent<T> {
-    
+
     private final int index1;
     private final int index2;
-    
+
     public SwapEvent(int index1, int index2) {
         this.index1 = index1;
         this.index2 = index2;
     }
-            
+
     @Override
     public void apply(T[] arr) {
         T temp = arr[index1];
