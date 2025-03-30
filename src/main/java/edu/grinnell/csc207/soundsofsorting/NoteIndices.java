bb8511f37721aa7827af906522c3e5a5a54dc59a
fui <fui@fui-ubuntu>
2025-03-30 17:15:43 -0500

    Finished the program!

--- src/main/java/edu/grinnell/csc207/soundsofsorting/NoteIndices.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/NoteIndices.java
@@ -3,41 +3,42 @@ package edu.grinnell.csc207.soundsofsorting;
 import java.util.Random;
 
 /**
- * A collection of indices into a Scale object.
- * These indices are the subject of the various sorting algorithms
- * in the program.
+ * A collection of indices into a Scale object. These indices are the subject of
+ * the various sorting algorithms in the program.
  */
 public class NoteIndices {
-    
+
     Integer[] noteIndicies;
     boolean[] highlighted;
-    
+
     /**
      * @param n the size of the scale object that these indices map into
      */
-    public NoteIndices(int n) {
-        this.highlighted = new boolean[n];
-        this.noteIndicies = new Integer[n];
+    public NoteIndices(Integer n) {
         initializeAndShuffle(n);
     }
-    
+
     /**
      * Reinitializes this collection of indices to map into a new scale object
-     * of the given size.  The collection is also shuffled to provide an
-     * initial starting point for the sorting process.
+     * of the given size. The collection is also shuffled to provide an initial
+     * starting point for the sorting process.
+     *
      * @param n the size of the scale object that these indices map into
      */
     public void initializeAndShuffle(Integer n) {
-        for (int i = 0; i < this.noteIndicies.length; i++){
+        this.highlighted = new boolean[n];
+        this.noteIndicies = new Integer[n];
+        for (int i = 0; i < this.noteIndicies.length; i++) {
             this.noteIndicies[i] = i;
         }
-        
+
         shuffle(this.noteIndicies);
     }
-    
+
     /**
      * fisher yates shuffle method
-     * @param array 
+     *
+     * @param array
      */
     public static void shuffle(Integer[] array) {
         Random random = new Random();
@@ -48,14 +49,17 @@ public class NoteIndices {
             array[i] = temp;
         }
     }
-    
-    /** @return the indices of this NoteIndices object */
-    public Integer[] getNotes() { 
+
+    /**
+     * @return the indices of this NoteIndices object
+     */
+    public Integer[] getNotes() {
         return noteIndicies;
     }
-    
+
     /**
      * Highlights the given index of the note array
+     *
      * @param index the index to highlight
      */
     public void highlightNote(Integer index) {
@@ -63,7 +67,7 @@ public class NoteIndices {
             this.highlighted[index] = true;
         }
     }
-    
+
     /**
      * @param index the index to check
      * @return true if the given index is highlighted
@@ -71,10 +75,12 @@ public class NoteIndices {
     public boolean isHighlighted(Integer index) {
         return index >= 0 && index < highlighted.length && highlighted[index];
     }
-    
-    /** Clears all highlighted indices from this collection */
+
+    /**
+     * Clears all highlighted indices from this collection
+     */
     public void clearAllHighlighted() {
-        for (int i = 0; i < this.highlighted.length; i++){
+        for (int i = 0; i < this.highlighted.length; i++) {
             this.highlighted[i] = false;
         }
     }
